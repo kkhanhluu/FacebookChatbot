1282b7ac53233f44d3b5b87829c5d37cde552260
Kim Khanh Luu <luukhanh96@gmail.com>
2019-05-08 15:08:40 +0200

    puppeteer not working on heroku

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function crawler() {
 }
 
 async function getLinks(url) {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto(url);
 
